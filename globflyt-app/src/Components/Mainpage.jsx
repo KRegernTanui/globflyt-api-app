6d055bd990c2fd338f6839a5ca9669e1159051e0
KRegernTanui <regerntanuikibet@gmail.com>
2025-05-17 18:10:56 +0300

    Added design contents to Mainpage,index.css & App.js

---
+++ globflyt-app/src/Components/Mainpage.jsx
@@ -0,0 +1,13 @@
+
+function Mainpage(){
+
+    return(
+        <div className="bg-slate-800 h-16 text-center text-5xl ">
+            <h>
+                <span style={{ color: 'black' }}>glob</span>
+                <span style={{ color: 'red' }}>flyt</span>
+            </h>
+        </div>
+    );
+}
+export default Mainpage;
